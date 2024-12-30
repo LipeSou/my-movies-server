f26b5f9125f0f6b9952e5f97afca785a1b119734
LipeSou <felipesoutomdt@gmail.com>
2024-12-30 20:16:02 -0300

    feat: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { UsersModule } from './users/users.module';
 import { WatchlistModule } from './watchlist/watchlist.module';
 import { WatchlistItemModule } from './watchlist-item/watchlist-item.module';
 import { EpisodeModule } from './episode/episode.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
+import { WatchlistUsersModule } from './watchlist-users/watchlist-users.module';
 
 @Module({
-  imports: [UsersModule, WatchlistModule, WatchlistItemModule, EpisodeModule],
+  imports: [
+    UsersModule,
+    WatchlistModule,
+    WatchlistItemModule,
+    EpisodeModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+    }),
+    WatchlistUsersModule,
+  ],
   controllers: [],
   providers: [],
 })
