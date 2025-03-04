cfa38480b2b4328315e0278a00b0c09458e68796
LipeSou <felipesoutomdt@gmail.com>
2025-03-04 18:18:04 -0300

    feat: public creat user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,11 +13,13 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { IsEmail } from 'class-validator';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @IsPublic()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
